d1d9bcecd676111d1c387ac749227b33e57f7444
Wilma Korpinen <Wilma@Wilma-MacBook-Pro.local>
2018-01-07 18:08:19 +0100

    images

---
+++ portfolio/interactive_animation/script.js
@@ -0,0 +1,143 @@
+let guccighost = document.querySelector(".guccighost");
+console.log(guccighost)
+
+1.
+
+let shakebutton = document.querySelector("#shakebutton");
+console.log(shakebutton)
+
+shakebutton.addEventListener("click", shakeclicked);
+
+function shakeclicked(){
+    console.log("hi");
+    guccighost.classList.toggle("shakecss");
+
+}
+
+
+2.
+
+let pulsebutton = document.querySelector("#pulsebutton");
+console.log(pulsebutton)
+
+
+pulsebutton.addEventListener("click", pulseclicked);
+
+function pulseclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("pulsecss");
+
+}
+
+
+3.
+
+let stretchbutton = document.querySelector("#stretchbutton");
+console.log(stretchbutton)
+
+
+stretchbutton.addEventListener("click", stretchclicked);
+
+function stretchclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("stretchcss");
+
+}
+
+
+4.
+
+let jumpbutton = document.querySelector("#jumpbutton");
+console.log(jumpbutton)
+
+
+jumpbutton.addEventListener("click", jumpclicked);
+
+function jumpclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("jumpcss");
+
+}
+
+5.
+
+let glowbutton = document.querySelector("#glowbutton");
+console.log(glowbutton)
+
+
+glowbutton.addEventListener("click", glowclicked);
+
+function glowclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("glowcss");
+
+}
+
+6.
+
+let slideinbutton = document.querySelector("#slideinbutton");
+console.log(slideinbutton)
+
+
+slideinbutton.addEventListener("click", slideinclicked);
+
+function slideinclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("slideincss");
+
+}
+
+7.
+
+let spinbutton = document.querySelector("#spinbutton");
+console.log(spinbutton)
+
+
+spinbutton.addEventListener("click", spinclicked);
+
+function spinclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("spincss");
+
+}
+
+
+8.
+let colormebutton = document.querySelector("#colormebutton");
+console.log(colormebutton)
+
+
+colormebutton.addEventListener("click", colormeclicked);
+
+function colormeclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("colormecss");
+
+}
+
+9.
+
+let zoomoutdownbutton = document.querySelector("#zoomoutdownbutton");
+console.log(zoomoutdownbutton)
+
+
+zoomoutdownbutton.addEventListener("click", zoomoutdownclicked);
+
+function zoomoutdownclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("zoomoutdowncss");
+
+}
+10.
+
+let zoominleftbutton = document.querySelector("#zoominleftbutton");
+console.log(zoominleftbutton)
+
+
+zoominleftbutton.addEventListener("click", zoominleftclicked);
+
+function zoominleftclicked(){
+    console.log("hi");
+    guccighost.classList.toggle ("zoominleftcss");
+
+}
---
+++ portfolio/simple_animation/script.js
@@ -0,0 +1,118 @@
+let bronze = document.querySelector("img.bronze");
+console.log(bronze)
+
+1.
+
+let shakebutton = document.querySelector("#shakebutton");
+console.log(shakebutton)
+
+shakebutton.addEventListener("click", shakeclicked);
+
+function shakeclicked(){
+    console.log("hi");
+    bronze.classList.toggle("shakecss");
+
+
+
+
+
+2.
+
+let stretchbutton = document.querySelector("#stretchbutton");
+console.log(stretchbutton)
+
+
+stretchbutton.addEventListener("click", stretchclicked);
+
+function stretchclicked(){
+    console.log("hi");
+    bronze.classList.toggle ("stretchcss");
+
+}
+
+
+3.
+
+let jumpbutton = document.querySelector("#jumpbutton");
+console.log(jumpbutton)
+
+
+jumpbutton.addEventListener("click", jumpclicked);
+
+function jumpclicked(){
+    console.log("hi");
+    bronze.classList.toggle ("jumpcss");
+
+}
+
+4.
+
+let glowbutton = document.querySelector("#glowbutton");
+console.log(glowbutton)
+
+
+glowbutton.addEventListener("click", glowclicked);
+
+function glowclicked(){
+    console.log("hi");
+    bronze.classList.toggle ("glowcss");
+
+}
+
+5.
+
+let slideinbutton = document.querySelector("#slideinbutton");
+console.log(slideinbutton)
+
+
+slideinbutton.addEventListener("click", slideinclicked);
+
+function slideinclicked(){
+    console.log("hi");
+    bronze.classList.toggle ("slideincss");
+
+}
+
+6.
+
+let spinbutton = document.querySelector("#spinbutton");
+console.log(spinbutton)
+
+
+spinbutton.addEventListener("click", spinclicked);
+
+function spinclicked(){
+    console.log("hi");
+    bronze.classList.toggle ("spincss");
+
+}
+
+
+
+
+7.
+
+let zoomoutdownbutton = document.querySelector("#zoomoutdownbutton");
+console.log(zoomoutdownbutton)
+
+
+zoomoutdownbutton.addEventListener("click", zoomoutdownclicked);
+
+function zoomoutdownclicked(){
+    console.log("hi ");
+    bronze.classList.toggle ("zoomoutdowncss");
+
+}
+8.
+
+let zoominleftbutton = document.querySelector("#zoominleftbutton");
+console.log(zoominleftbutton)
+
+
+zoominleftbutton.addEventListener("click", zoominleftclicked);
+
+function zoominleftclicked(){
+    console.log("hi");
+    bronze.classList.toggle ("zoominleftcss");
+
+}
